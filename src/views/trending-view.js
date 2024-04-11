8fd52697d150971ca66878b110381a55bd31bded
Petko Petkov <petkozlatilov1234@gmail.com>
2024-04-12 00:07:47 +0300

    changed the getData function to be responsible for different urls

--- src/views/trending-view.js
+++ src/views/trending-view.js
@@ -1,8 +1,8 @@
 const key = "OjO2azlZWV1Y4SABaT4Nuw1bsaHIJKON";
 const key2 = "mvRJrWupzEx2WaJ8TDgcGOOXXcPXH3mF";
-const url = `https://api.giphy.com/v1/gifs/trending?api_key=${key}&limit=20`;
+const url = `https://api.giphy.com/v1/gifs/trending?api_key=${key2}&limit=20`;
 
-async function getData() {
+export async function getData(url) {
   try {
     const response = await fetch(url);
     const data = await response.json();
@@ -14,12 +14,14 @@ async function getData() {
 
 export async function renderHome() {
   try {
-    const data = await getData();
+    const data = await getData(url);
     const div = document.createElement("div");
     div.classList = "trendingContainer";
     for (let el of data.data) {
       const gifImg = document.createElement("img");
       gifImg.src = el.images.fixed_height.url;
+      gifImg.id = el.id;
+      gifImg.className = "giphyImg";
       div.appendChild(gifImg);
     }
     return div;
