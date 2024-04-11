dfe4120756bc9a2cf70d1c1f25fa85272b736742
todor-savov <todor.savov.a58@learn.telerikacademy.com>
2024-04-11 21:49:02 +0300

    removed unnecessary code from the click listener in index.js

--- src/index.js
+++ src/index.js
@@ -8,28 +8,11 @@ document.addEventListener("DOMContentLoaded", async () => {
   container.appendChild(renderedContent);
 
   // add global listener for "click" events -> filter clicks by element id
-  document.addEventListener("click", async (event) => {
+  document.addEventListener("click", (event) => {
 
     if (event.target.id === "searchBtn") {
-
-          const searchString = document.querySelector("input#search").value;
-          searchByString(searchString);
-    
-          const html = `
-          <div id="allGifsContainer">
-              ${jsonData.data.map(item => {
-                return `<div id="gifContainer">
-                            <img src="${item.images.fixed_height.webp}">
-                        </div>`;
-                })
-                .join('\n')
-              }
-          </div>`;
-
-          document.querySelector("div#container").innerHTML = html;
-
-      
-
+      const searchString = document.querySelector("input#search").value;
+      document.querySelector("div#container").innerHTML = searchByString(searchString);
     }
 
   });
