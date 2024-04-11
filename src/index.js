03662497a142cc913d32a66894ae10fc7b7fd2bf
Petko Petkov <petkozlatilov1234@gmail.com>
2024-04-12 00:06:24 +0300

    added eventListener for click that renders my details div on click

--- src/index.js
+++ src/index.js
@@ -1,41 +1,50 @@
 import { renderHome, trendingTitle } from "./views/trending-view.js";
+import { renderDetails, Details } from "./views/display-details.js";
 
 document.addEventListener("DOMContentLoaded", async () => {
+  const detailsView = Details();
+  const renderedContent = await renderHome();
   const container = document.querySelector("#container");
   container.appendChild(trendingTitle());
-  const renderedContent = await renderHome();
   container.appendChild(renderedContent);
+  container.appendChild(detailsView);
+  document.addEventListener("click", async (e) => {
+    if (e.target.classList.contains("giphyImg")) {
+      const detailsDiv = document.querySelector(".divDetails");
+      await renderDetails(e.target.id, detailsDiv);
+      detailsDiv.style.display = "block";
+    } else {
+      const detailsDiv = document.querySelector(".divDetails");
+      detailsDiv.style.display = "none";
+    }
+  });
 
   // add global listener for "click" events -> filter clicks by element id
   document.addEventListener("click", async (event) => {
-
     if (event.target.id === "searchBtn") {
-
       try {
-          const api_key = 'OjO2azlZWV1Y4SABaT4Nuw1bsaHIJKON';
-          const searchString = document.querySelector("input#search").value;
-          const data = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=${api_key}&q=${searchString}&limit=5`);
-          const jsonData = await data.json();
-    
-          const html = `
+        const api_key = "OjO2azlZWV1Y4SABaT4Nuw1bsaHIJKON";
+        const searchString = document.querySelector("input#search").value;
+        const data = await fetch(
+          `https://api.giphy.com/v1/gifs/search?api_key=${api_key}&q=${searchString}&limit=5`
+        );
+        const jsonData = await data.json();
+
+        const html = `
           <div id="allGifsContainer">
-              ${jsonData.data.map(item => {
-                return `<div id="gifContainer">
+              ${jsonData.data
+                .map((item) => {
+                  return `<div id="gifContainer">
                             <img src="${item.images.fixed_height.webp}">
                         </div>`;
                 })
-                .join('\n')
-              }
+                .join("\n")}
           </div>`;
 
-          document.querySelector("div#container").innerHTML = html;
-
+        document.querySelector("div#container").innerHTML = html;
       } catch (e) {
         console.log(e.message);
       }
-
     }
-
   });
-
 });
