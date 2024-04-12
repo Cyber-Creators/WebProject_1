ee8ee5ea5410d4fb17ae3567fef37802be7c5a91
Petko Petkov <petkozlatilov1234@gmail.com>
2024-04-12 10:49:56 +0300

    added second api key in case the first one dosnt work

--- src/common/constants.js
+++ src/common/constants.js
@@ -1,13 +1,14 @@
-export const API_KEY = 'OjO2azlZWV1Y4SABaT4Nuw1bsaHIJKON';
+export const API_KEY = "OjO2azlZWV1Y4SABaT4Nuw1bsaHIJKON";
+export const API_KEY2 = "mvRJrWupzEx2WaJ8TDgcGOOXXcPXH3mF";
 
-export const CATEGORIES = 'categories';
+export const CATEGORIES = "categories";
 
-export const FAVORITES = 'favorites';
+export const FAVORITES = "favorites";
 
-export const ABOUT = 'about';
+export const ABOUT = "about";
 
-export const CONTAINER_SELECTOR = '#container';
+export const CONTAINER_SELECTOR = "#container";
 
-export const FULL_HEART = '❤';
+export const FULL_HEART = "❤";
 
-export const EMPTY_HEART = '♡';
+export const EMPTY_HEART = "♡";
