dd1dc2f053bd122bc04f19232c29836cf54eb0ee
todor-savov <todor.savov.a58@learn.telerikacademy.com>
2024-04-12 15:08:06 +0300

    added generateUploadForm() function to upload-view.js

---
+++ src/views/upload-view.js
@@ -0,0 +1,10 @@
+export const generateUploadForm = async () => {
+
+    return `<div id="myUploadForm">
+                <form action="" method="post" enctype="multipart/form-data">
+                    <input type="file" id="" name="">
+                    <button type="submit">Upload</button>
+                </form>
+            </div>`;
+
+};
