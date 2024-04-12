2c883c0c167bf8b3cadd25e87c6a15dec99d29c3
Petko Petkov <petkozlatilov1234@gmail.com>
2024-04-12 23:03:07 +0300

    if no name provided to be NO name

--- src/views/display-details.js
+++ src/views/display-details.js
@@ -8,14 +8,18 @@ export function Details() {
 }
 
 export async function renderDetails(id, div) {
-  const url = `https://api.giphy.com/v1/gifs/${id}?api_key=${API_KEY}`;
+  const url = `https://api.giphy.com/v1/gifs/${id}?api_key=${API_KEY2}`;
   const data = await getData(url);
   div.innerHTML = `
   <img src="${data.data.images.original.url} alt="" />
   <h3>${data.data.title}</h3>
   <hr id="hr" />
-  <p>Username: <span id="name">${data.data.username}</span></p>
+  <p>Username: <span id="name">${
+    data.data.username ? data.data.username : "No name"
+  }</span></p>
   <p>rating: ${data.data.rating}</p>
-  <a href=${data.data.embed_url} target="_blank"><i class="fa-solid fa-up-right-from-square fa-lg" style="color: #63E6BE;"></i>  Giffy</a>
+  <a href=${
+    data.data.embed_url
+  } target="_blank"><i class="fa-solid fa-up-right-from-square fa-lg" style="color: #63E6BE;"></i>  Giffy</a>
   `;
 }
