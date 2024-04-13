266c0705a67ecaa9b26366414b18f7634160b209
Petko Petkov <petkozlatilov1234@gmail.com>
2024-04-13 19:55:55 +0300

    Just added some html text to the div to look better in the page. Didnt tuch the funcionality of the code

--- src/views/upload-view.js
+++ src/views/upload-view.js
@@ -1,10 +1,9 @@
 export const generateUploadForm = async () => {
-
-    return `<div id="uploadForm">
+  return `<div id="uploadForm">
                 <form id="myUploadForm">
+                <label for="arquivo">Choose a file:</label>
                     <input type="file" id="file" name="file">
                     <button type="submit">Upload</button>
                 </form>
             </div>`;
-
 };
