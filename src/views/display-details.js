7af42415430d7cfb63f87f3640ad4b1ff249f9b5
Petko Petkov <petkozlatilov1234@gmail.com>
2024-04-14 11:36:43 +0300

    Added documentation to the code

--- src/views/display-details.js
+++ src/views/display-details.js
@@ -1,14 +1,26 @@
 import { getDetailsData } from "../requests/request-service.js";
 import { EMPTY_HEART } from "../common/constants.js";
 
+/**
+ * Creates and returns a div element for displaying details.
+ * @returns {HTMLElement} A div element with class name "divDetails".
+ */
 export function Details() {
   const divDetail = document.createElement("div");
   divDetail.className = "divDetails";
   return divDetail;
 }
 
+/**
+ * Renders the details of a specific GIF.
+ * @param {string} id - The ID of the GIF.
+ * @param {HTMLElement} div - The div element where the details will be rendered.
+ */
 export async function renderDetails(id, div) {
   try {
+    /**
+     * @property {Object} data - The data from the API.
+     */
     const data = await getDetailsData(id);
     div.innerHTML = `
       <img src="${data.url}" alt="" />
