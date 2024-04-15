7e56b1c0d3a87601fc13c3757a9686bff00a69ab
Petko Petkov <petkozlatilov1234@gmail.com>
2024-04-15 19:31:56 +0300

    added more details

--- src/views/about-view.js
+++ src/views/about-view.js
@@ -3,15 +3,27 @@ import { card } from "../events/cards.js";
 export const aboutView = () => {
   return `
   <div id="aboutContainer">
-     ${card()}
+     ${card(
+       "./images/",
+       "Todor Savov",
+       "https://github.com/Petkov-Petko",
+       "https://www.instagram.com/petko_zlatilov/",
+       "https://www.facebook.com/petko.zl/"
+     )}
     ${card(
-      "../../images/Ipetko8 (1).JPG",
+      "./images/Ipetko8 (1).JPG",
       "Petko Petkov",
       "https://github.com/Petkov-Petko",
       "https://www.instagram.com/petko_zlatilov/",
       "https://www.facebook.com/petko.zl/"
     )}
-    ${card()}
+    ${card(
+      "./images/",
+      "Anton Miltenov",
+      "https://github.com/Petkov-Petko",
+      "https://www.instagram.com/petko_zlatilov/",
+      "https://www.facebook.com/petko.zl/"
+    )}
   </div>
 `;
 };
