d7590a6c7ce0b7b68390640f65300819b09fb2a5
Petko Petkov <petkozlatilov1234@gmail.com>
2024-04-15 10:08:40 +0300

    Add favorites.js module for managing favorite GIFs

---
+++ src/events/favorites.js
@@ -0,0 +1,17 @@
+let favorites = JSON.parse(localStorage.getItem("favorites")) || [];
+
+export const addFavorite = (favoriteId) => {
+  if (favorites.find((id) => id === favoriteId)) {
+    return;
+  }
+
+  favorites.push(favoriteId);
+  localStorage.setItem("favorites", JSON.stringify(favorites));
+};
+
+export const removeFavorite = (favoriteId) => {
+  favorites = favorites.filter((id) => id !== favoriteId);
+  localStorage.setItem("favorites", JSON.stringify(favorites));
+};
+
+export const getFavorites = () => [...favorites];
