aded58f0ffdba0c9bc89d847f53d14c6d3f52d1d
Petko Petkov <petkozlatilov1234@gmail.com>
2024-04-15 19:54:52 +0300

    added imgs to test

--- src/views/about-view.js
+++ src/views/about-view.js
@@ -10,21 +10,21 @@ export const aboutView = () => {
   return `
   <div id="aboutContainer">
      ${card(
-       "./images/",
+       "../../images/Ipetko8 (1).JPG",
        "Todor Savov",
        "https://github.com/Petkov-Petko",
        "https://www.instagram.com/petko_zlatilov/",
        "https://www.facebook.com/petko.zl/"
      )}
     ${card(
-      "./images/Ipetko8 (1).JPG",
+      "../../images/Ipetko8 (1).JPG",
       "Petko Petkov",
       "https://github.com/Petkov-Petko",
       "https://www.instagram.com/petko_zlatilov/",
       "https://www.facebook.com/petko.zl/"
     )}
     ${card(
-      "./images/",
+      "../../images/Ipetko8 (1).JPG",
       "Anton Miltenov",
       "https://github.com/Petkov-Petko",
       "https://www.instagram.com/petko_zlatilov/",
