9544ecfa22cd56e0ba1c63f93f002ecbcba2f5bc
todor-savov <todor.savov.a58@learn.telerikacademy.com>
2024-04-15 12:57:57 +0300

    deleted navigation-events.js and search-events.js

--- src/index.js
+++ src/index.js
@@ -77,13 +77,14 @@ document.addEventListener("DOMContentLoaded", async () => {
     }
     /* favorites */
     if (event.target.classList.contains("favorite")) {
-      const id = event.target.dataset.id;
-      toggleFavoriteStatus(id);
+      const movieId = event.target.dataset.movieId;
+      toggleFavoriteStatus(movieId);
     }
 
     if (event.target.id === "favorites") {
       event.preventDefault();
       const favorites = JSON.parse(localStorage.getItem("favorites"));
+      console.log(favorites);
       document.querySelector("div#container").innerHTML =
         favorites.length !== 0
           ? await getFavoriteGifsById(favorites.join(","))
