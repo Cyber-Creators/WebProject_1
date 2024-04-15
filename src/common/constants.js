4f9128d60f367e1187fe379aec782f114b9f5a5f
todor-savov <todor.savov.a58@learn.telerikacademy.com>
2024-04-15 15:44:16 +0300

    removed unnecessary lines from constants.js

--- src/common/constants.js
+++ src/common/constants.js
@@ -1,14 +1,4 @@
- export const API_KEY = "KtzPyAoJb9AgzuM037P7pcTReo7AnuBZ";
+export const API_KEY = "KtzPyAoJb9AgzuM037P7pcTReo7AnuBZ";
 //export const API_KEY = "mvRJrWupzEx2WaJ8TDgcGOOXXcPXH3mF";
-
-export const CATEGORIES = "categories";
-
-export const FAVORITES = "favorites";
-
-export const ABOUT = "about";
-
-export const CONTAINER_SELECTOR = "#container";
-
 export const FULL_HEART = "❤";
-
 export const EMPTY_HEART = "♡";
