74d89babff4adb5b1ffece99d13f7922801b301f
Petko Petkov <petkozlatilov1234@gmail.com>
2024-04-16 22:13:43 +0300

    changed movieId ot favoriteId

--- src/events/favorites-events.js
+++ src/events/favorites-events.js
@@ -2,25 +2,25 @@ import { EMPTY_HEART, FULL_HEART } from "../common/constants.js";
 import { addFavorite, getFavorites, removeFavorite } from "./favorites.js";
 import { q } from "./helpers.js";
 
-export const toggleFavoriteStatus = (movieId) => {
+export const toggleFavoriteStatus = (favoriteId) => {
   const favorites = getFavorites();
-  const heartSpan = q(`span[data-movie-id="${movieId}"]`);
+  const heartSpan = q(`span[data-movie-id="${favoriteId}"]`);
 
-  if (favorites.includes(movieId)) {
-    removeFavorite(movieId);
+  if (favorites.includes(favoriteId)) {
+    removeFavorite(favoriteId);
     heartSpan.classList.remove("active");
     heartSpan.innerHTML = EMPTY_HEART;
   } else {
-    addFavorite(movieId);
+    addFavorite(favoriteId);
     heartSpan.classList.add("active");
     heartSpan.innerHTML = FULL_HEART;
   }
 };
 
-export const renderFavoriteStatus = (movieId) => {
+export const renderFavoriteStatus = (favoriteId) => {
   const favorites = getFavorites();
 
-  return favorites.includes(movieId)
-    ? `<span class="favorite active" data-movie-id="${movieId}">${FULL_HEART}</span>`
-    : `<span class="favorite" data-movie-id="${movieId}">${EMPTY_HEART}</span>`;
+  return favorites.includes(favoriteId)
+    ? `<span class="favorite active" data-movie-id="${favoriteId}">${FULL_HEART}</span>`
+    : `<span class="favorite" data-movie-id="${favoriteId}">${EMPTY_HEART}</span>`;
 };
