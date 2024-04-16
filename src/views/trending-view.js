1659b2f76646382b294c6448cde0513fb5672c60
Petko Petkov <petkozlatilov1234@gmail.com>
2024-04-16 22:17:24 +0300

    Added js doc

--- src/views/trending-view.js
+++ src/views/trending-view.js
@@ -1,11 +1,17 @@
 import { getTrendingData } from "../requests/request-service.js";
 
+/**
+ * Creates a trigger element for loading more trending GIFs and sets up an IntersectionObserver to load more GIFs when the trigger is intersected.
+ * @param {HTMLElement} container - The container element where the GIFs are being displayed.
+ * @returns {HTMLElement} The container element with the trigger element appended.
+ * @throws Will throw an error if there is a problem rendering the home page.
+ */
 export async function renderHome() {
   try {
     const data = await getTrendingData();
     const container = document.createElement("div");
     container.classList.add("trendingContainer");
-    
+
     for (let el of data) {
       const img = document.createElement("img");
       img.src = el.url;
@@ -36,6 +42,10 @@ export async function renderHome() {
   }
 }
 
+/**
+ * Creates a title element for the trending section.
+ * @returns {Node} The first child of a div element containing the trending title HTML.
+ */
 export function trendingTitle() {
   const html = `
     <h3 class="trending">
@@ -47,9 +57,14 @@ export function trendingTitle() {
   return div.firstChild;
 }
 
+/**
+ * Loads more trending GIFs into the container.
+ * @param {HTMLElement} container - The container element where the GIFs are being displayed.
+ * @throws Will throw an error if there is a problem loading the trending data.
+ */
 async function loadMoreTrendingGIFs(container) {
   try {
-    const currentCount = container.querySelectorAll('.giphyImg').length;
+    const currentCount = container.querySelectorAll(".giphyImg").length;
     const newData = await getTrendingData(currentCount);
 
     for (let el of newData) {
