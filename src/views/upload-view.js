f1178811cdaaaf71c8056b5f6e312b6ca6423be4
todor-savov <todor.savov.a58@learn.telerikacademy.com>
2024-04-16 13:10:40 +0300

    Added additional DIV container inside div#uploadForm to show/hide spinner

--- src/views/upload-view.js
+++ src/views/upload-view.js
@@ -1,5 +1,6 @@
 export const generateUploadForm = async () => {
   return `<div id="uploadForm">
+            <div id="spinner" class="spinner hidden"></div>
                 <form id="myUploadForm">
                 <label for="arquivo">Choose a file:</label>
                     <input type="file" id="file" name="file">
