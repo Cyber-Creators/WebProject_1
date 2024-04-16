12fc1a0d1fda76bdde5b4f13f78ddf62fea79d75
todor-savov <todor.savov.a58@learn.telerikacademy.com>
2024-04-16 16:11:57 +0300

    changed API Key

--- src/common/constants.js
+++ src/common/constants.js
@@ -1,4 +1,4 @@
-export const API_KEY = "KtzPyAoJb9AgzuM037P7pcTReo7AnuBZ";
-//export const API_KEY = "mvRJrWupzEx2WaJ8TDgcGOOXXcPXH3mF";
+//export const API_KEY = "KtzPyAoJb9AgzuM037P7pcTReo7AnuBZ";
+export const API_KEY = "mvRJrWupzEx2WaJ8TDgcGOOXXcPXH3mF";
 export const FULL_HEART = "❤";
 export const EMPTY_HEART = "♡";
