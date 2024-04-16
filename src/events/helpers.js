cf356330ee9932b04e8614205d056e253d79d44d
Petko Petkov <petkozlatilov1234@gmail.com>
2024-04-16 21:04:39 +0300

    added js doc

--- src/events/helpers.js
+++ src/events/helpers.js
@@ -1,27 +1,26 @@
 /**
  * Shorthand for document.querySelector
- * @param {string} selector 
+ * @param {string} selector
  * @returns {Element}
  */
- export const q = (selector) => document.querySelector(selector);
+export const q = (selector) => document.querySelector(selector);
 
- /**
-  * Shorthand for document.querySelectorAll
-  * @param {string} selector 
-  * @returns {NodeLists<Element>}
-  */
- export const qs = (selector) => document.querySelectorAll(selector);
- 
- export const setActiveNav = (page) => {
-   const navs = qs('a.nav-link');
- 
-   Array
-     .from(navs)
-     .forEach(element => element
-       .getAttribute('data-page') === page
-       ? element.classList.add('active')
-       : element.classList.remove('active')
-       );
- };
- 
- 
+/**
+ * Shorthand for document.querySelectorAll
+ * @param {string} selector
+ * @returns {NodeLists<Element>}
+ */
+export const qs = (selector) => document.querySelectorAll(selector);
+/**
+ * Sets the active navigation link based on the specified page.
+ * @param {string} page - The page to set as active.
+ */
+export const setActiveNav = (page) => {
+  const navs = qs("a.nav-link");
+
+  Array.from(navs).forEach((element) =>
+    element.getAttribute("data-page") === page
+      ? element.classList.add("active")
+      : element.classList.remove("active")
+  );
+};
